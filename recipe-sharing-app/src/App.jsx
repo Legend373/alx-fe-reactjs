fc511dc932759eacca8c21253cc1da7b586f2c4b
Legend373 <164746195+Legend373@users.noreply.github.com>
2025-03-01 05:14:48 -0300

    cor2

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { RecipeList } from './components/RecipeList.jsx'
-import { AddRecipeForm } from './components/AddRecipeForm.jsx'
+import RecipeList from './components/RecipeList.jsx'
+import AddRecipeForm from './components/AddRecipeForm.jsx'
 import './App.css'
 
 function App() {
