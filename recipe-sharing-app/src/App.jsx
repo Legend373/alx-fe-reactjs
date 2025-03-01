5a4a63762657637b7e4fc34ba371a7887518f7b7
Legend373 <164746195+Legend373@users.noreply.github.com>
2025-03-01 05:07:25 -0300

    setup1

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { RecipeList } from './components/RecipeList.jsx'
+import { AddRecipeForm } from './components/AddRecipeForm.jsx'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
