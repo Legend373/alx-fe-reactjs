910f3cbe745b81d105c1b1654edfddcd79b21d13
Legend373 <164746195+Legend373@users.noreply.github.com>
2025-03-01 06:04:55 -0300

    cor4

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import RecipeList from './components/RecipeList.jsx'
-import AddRecipeForm from './components/AddRecipeForm.jsx'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RecipeDetails from "./RecipeDetails";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
-
-
   return (
-    <>
-      <AddRecipeForm />
-      <RecipeList />
-    </>
-  )
+    <Router>
+      <div>
+        <h1>Recipe Manager</h1>
+        <AddRecipeForm />
+        <RecipeList />
+        <Routes>
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
