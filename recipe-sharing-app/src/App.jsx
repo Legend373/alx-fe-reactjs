732ba3144d0c0b26f65336ed26660d27446f7799
Legend373 <164746195+Legend373@users.noreply.github.com>
2025-03-01 07:20:41 -0300

    setup3

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import RecipeDetails from "./RecipeDetails";
+import RecipeDetails from "./components/RecipeDetails";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <Router>
       <div>
         <h1>Recipe Manager</h1>
+        <SearchBar />
         <AddRecipeForm />
         <RecipeList />
         <Routes>
