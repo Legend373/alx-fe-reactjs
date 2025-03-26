c90ce4d39e4d119122043372f2cd4831defdc14b
Legend373 <164746195+Legend373@users.noreply.github.com>
2025-03-26 08:55:22 -0300

    cor1

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
-import data from "../data.json"; // Import JSON directly
+import data from "../data.json";
+import { NavLink } from "react-router";// Import JSON directly
+import RecipeDetail from "./RecipeDetail";
 
 const HomePage = () => {
     const [recipes, setRecipes] = useState([]);
@@ -13,18 +15,20 @@ const HomePage = () => {
             <h1 className="text-2xl font-bold mb-4 text-center">Recipe List</h1>
             <div className="grid md:grid-cols-2 gap-4">
                 {recipes.map((recipe) => (
-                    <div key={recipe.id} className="bg-white shadow-md rounded-lg p-4">
-                        <img
-                            src={recipe.image}
-                            alt={recipe.title}
-                            className="w-full h-40 object-cover rounded-md mb-2"
-                        />
-                        <h2 className="text-xl font-semibold">{recipe.title}</h2>
-                        <p className="text-gray-600">{recipe.summary}</p>
-                    </div>
+                    <NavLink NavLink to="/recipe/:id">
+                        <div key={recipe.id} className="bg-white shadow-md rounded-lg p-4">
+                            <img
+                                src={recipe.image}
+                                alt={recipe.title}
+                                className="w-full h-40 object-cover rounded-md mb-2"
+                            />
+                            <h2 className="text-xl font-semibold">{recipe.title}</h2>
+                            <p className="text-gray-600">{recipe.summary}</p>
+                        </div>
+                    </NavLink>
                 ))}
             </div>
-        </div>
+        </div >
     );
 };
 
